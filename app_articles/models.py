ac7d854ff857b8eb86af9b4d7ed9422297265407
WitchInTheHandkerchief <ilimkanu@gmail.com>
2024-02-03 18:35:40 +0600

    feat: Add models

--- app_articles/models.py
+++ app_articles/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.utils.translation import gettext_lazy as _
 
-# Create your models here.
+
+class Articles(models.Model):
+    title = models.CharField(max_length=255)
+    short_description = models.TextField()
+    description = models.TextField()
+    date_created = models.DateField()
+
+    class Meta:
+        verbose_name = _("article")
+        verbose_name_plural = _("articles")
+
+    def __str__(self):
+        return f'{self.title}'
